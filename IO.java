7cd542261de48158e9e4f012d774b1325ab3a9db
NightWng <gibranakmal@hotmail.com>
2021-04-05 19:56:15 -0600

    IO class update
    Added output functionality to the existing IO class. Formats a text file after given the required inputs based on the example text file format.

--- IO.java
+++ IO.java
@@ -1,7 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.stream.Sink.ChainedReference;
+//import java.util.stream.Sink.ChainedReference;
 
-public class IO{
+
+
+
+
+
+/**
+ * IO class handles all input and output activities of the program
+ * Mainly involves reading from the data base
+ * taking in processed information to be formatted into an output file
+ */	
+
+public class IO
+{
     private boolean combFound;
     private String category;
     private String type;
@@ -84,5 +101,78 @@ public class IO{
     //check data
     
 
-
+    //data member for output handling
+	public FileWriter output;
+	
+	/**
+	 * No return type. Only performs output actions
+	 * Takes in required information and formats it onto an output file 
+	 * Following the specified format of the given example
+	 * Overloaded method in case order cannot be fulfilled
+	 * @param furnitureType is the piece of furniture that the customer wants e.g(chair)
+	 * @param units is how many of that specific type of furniture the user is requesting
+	 * @param ID is an array list consisting of all the ID's of the pieces used to fulfill a VALID COMPLETE order
+	 * @param totalPrice is the total price of an order that can be fulfilled
+	 */	
+	public void output(String furnitureType, int units, ArrayList<String> ID,  double totalPrice) 
+	{
+		
+		try 
+		{
+			//the output FileWriter will output to a text file called "output.txt"			
+			this.output = new FileWriter("orderform.txt", false); //the second parameter of false tells it to overwrite an existing file of that name if it exists
+			
+			this.output.write("Furniture Order Form\n\n");
+			this.output.write("Faculty Name: \n");
+			this.output.write("Contact: \n");
+			this.output.write("Date: \n\n");
+			
+			this.output.write("Original Request: " + furnitureType + ", /" + units + "\n\n");
+			
+			this.output.write("Items Ordered\n");
+			
+			for(int counter = 0; counter < ID.size(); counter++) 
+			{
+				this.output.write("ID: " + ID.get(counter) + "\n");
+			}
+			
+			this.output.write("\nTotal Price: $" + "totalPrice");
+			
+			
+		}
+		catch(IOException e) 	//exception for output file
+		{
+			//System.out.println("error occurred with the output file");
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//overloaded method
+	//if only these two arguments are supplied, this means the order was unsuccessful
+	public void output(String furnitureType, int units) 
+	{
+		try 
+		{
+			//the output FileWriter will output to a text file called "output.txt"			
+			this.output = new FileWriter("output.txt", false); //the second parameter of false tells it to overwrite an existing file
+			
+			this.output.write("User request: " + furnitureType + ", " + units + "\n" );
+			this.output.write("Output " + "Order cannot be fulfilled based on current inventory. Suggested manufacturers are: \n\n");
+			this.output.write("Office Furnishings\n");
+			this.output.write("Chairs R Us\n");
+			this.output.write("Furniture Goods\n");
+			this.output.write("Fine Office Supplies\n\n\n");
+			this.output.write("Sorry we didn't have what you were looking for, please come back next time!");
+			
+		}
+		catch(IOException e) 	//exception for output file
+		{
+			//System.out.println("error occurred with the output file")
+			e.printStackTrace();
+		}	
+		
+	}
 }
+
+//end of class declaration
